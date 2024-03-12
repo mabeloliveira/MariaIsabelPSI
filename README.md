8ec12364d0df37f1fb9c804ffc72c9c35121be03
mabeloliveira <131927256+mabeloliveira@users.noreply.github.com>
2024-03-12 14:12:03 +0000

    Aula do dia 12/03 :)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MariaIsabelPSI
+Aula de 12/03
+
+1 - Criar a pasta static na raiz do projeto (integra o V do modelo MVC - Model View Control. View no MVC: a camada de interação com o usúario é o que o usúario "visualiza" no sistema)
+
+2 - Buscar uma imagem na internet, salvar no computador 
+
+3 - Fazer upload da imagem no VSCode e inserir na pasta static
+
+4 - Criar subpastas por tipo de arquivo dentro da pasta static
--- app.py
+++ app.py
@@ -1,18 +1,48 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#Aula 05/03 e 12/03: 
+
 @app.route('/')
-def index(): 
-    return 'Compre sua faixa da TOLF' 
+def index():
+    return render_template('index.html')
 
 @app.route('/compras')
 def compras():
-    #return '<ul><li>Arroz</li></ul>'   
-    return render_template('compras.html')
+    #return '<ul><li>Arroz</li></ul>'
+    return render_template('compras.html', item1 = 'Farinha', item2='Cuscuz')
 
 @app.route('/mercados')
 def mercados():
     return render_template('mercados.html')
+
+'''
+@app.route('/gastos')
+def gastos():
+    mes = 'Fevereiro'
+    valor = '843,00'
+    return render_template('gastos.html', a = mes, b = valor)
+'''
+
+@app.route('/gastos/<mes>')
+def gastos(mes):
+    return render_template('gastos.html', a = mes)
+
+@app.route('/q1_pedirpizza/<sabor>')
+def q1_pedirpizza(sabor):
+    return render_template('q1_pedirpizza.html' , sabor=sabor )
+
+@app.route('/dobro', defaults={"n":0})
+@app.route('/dobro/<int:n>')
+@app.route('/dobro/<float:n>')
+def dobro(n):
+    resultado = 2*n
+    return render_template('dobro.html', n=n, resultado=resultado)
+
+@app.route('/perfil', defaults = {'nome':'anonimo'})
+@app.route('/perfil/<nome>')
+def perfil (nome):
+    return render_template('perfil.html', nome = nome)
 
 if __name__                                                                                                                                                                                                                                                                                                                    == 'main':
     app.run()
